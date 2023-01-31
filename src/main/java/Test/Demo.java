71baaad8b29612b66476ef809279d2ae06455fc5
Srikathir124 <srikathir124@gmail.com>
2023-01-31 15:59:48 +0530

    updated hardcoded xpaths to external properties file

--- src/main/java/Test/Demo.java
+++ src/main/java/Test/Demo.java
@@ -39,60 +39,60 @@ public class Demo {
 	
 	@Test
 	public void RunTest() {
-		Duration fifteenSeconds = Duration.ofSeconds(15);
+		Duration tenSeconds = Duration.ofSeconds(10);
 		Duration oneSecond = Duration.ofSeconds(1);
-		Wait<EdgeDriver> wait = new FluentWait(driver).withTimeout(fifteenSeconds).pollingEvery(oneSecond)
+		Wait<EdgeDriver> wait = new FluentWait(driver).withTimeout(tenSeconds).pollingEvery(oneSecond)
 				.ignoring(NoSuchElementException.class);
 		Date date = new Date();
 
 		// Logging IN
 		driver.findElement(By.linkText("Log in")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Enter email']")));
-		driver.findElement(By.xpath("//input[@placeholder='Enter email']")).sendKeys(prop.getProperty("username"));
-		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(prop.getProperty("loginPage.username.xpath"))));
+		driver.findElement(By.xpath(prop.getProperty("loginPage.username.xpath"))).sendKeys(prop.getProperty("username"));
+		driver.findElement(By.xpath(prop.getProperty("loginPage.continue.xpath"))).click();
 
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Enter password']")));
-		driver.findElement(By.xpath("//input[@placeholder='Enter password']")).sendKeys(prop.getProperty("password"));
-		driver.findElement(By.xpath("//span[text()='Log in']")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(prop.getProperty("loginPage.password.xpath"))));
+		driver.findElement(By.xpath(prop.getProperty("loginPage.password.xpath"))).sendKeys(prop.getProperty("password"));
+		driver.findElement(By.xpath(prop.getProperty("loginPage.login.xpath"))).click();
 
 		// Creating New Board
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Create new board']")));
-		driver.findElement(By.xpath("//span[text()='Create new board']")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(prop.getProperty("homePage.createBoard.xpath"))));
+		driver.findElement(By.xpath(prop.getProperty("homePage.createBoard.xpath"))).click();
 
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[text()='Board title']")));
-		driver.findElement(By.xpath("//div[text()='Board title']/following-sibling::input"))
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(prop.getProperty("homePage.boardTitle.xpath"))));
+		driver.findElement(By.xpath(prop.getProperty("homePage.boardTitle.xpath")))
 				.sendKeys("Board " + date.toString());
-		driver.findElement(By.xpath("//button[@data-testid='create-board-submit-button']")).click();
+		driver.findElement(By.xpath(prop.getProperty("homePage.createBoardSubmit.xpath"))).click();
 
 		// Adding List
 		wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("//span[text()='Add a list']/ancestor::a/following-sibling::input")));
-		driver.findElement(By.xpath("//span[text()='Add a list']/ancestor::a/following-sibling::input"))
+				.elementToBeClickable(By.xpath(prop.getProperty("listPage.listAHeading.xpath"))));
+		driver.findElement(By.xpath(prop.getProperty("listPage.listAHeading.xpath")))
 				.sendKeys("List A");
-		driver.findElement(By.xpath("//input[@value='Add list']")).click();
-		driver.findElement(By.xpath("//input[@placeholder='Enter list title…']")).sendKeys("List B");
-		driver.findElement(By.xpath("//input[@value='Add list']")).click();
+		driver.findElement(By.xpath(prop.getProperty("listPage.addList.xpath"))).click();
+		driver.findElement(By.xpath(prop.getProperty("listPage.listBHeading.xpath"))).sendKeys("List B");
+		driver.findElement(By.xpath(prop.getProperty("listPage.addList.xpath"))).click();
 
 		// Adding List Item
-		driver.findElement(By.xpath("//h2[text()='List A']/ancestor::div[1]/following-sibling::div[2]/a[1]")).click();
+		driver.findElement(By.xpath(prop.getProperty("listPage.addListItem.xpath"))).click();
 		driver.manage().timeouts().implicitlyWait(oneSecond);
-		driver.findElement(By.xpath("//h2[text()='List A']/ancestor::div[1]/following-sibling::div[1]//textarea[1]"))
+		driver.findElement(By.xpath(prop.getProperty("listPage.listItemTextbox.xpath")))
 				.sendKeys(date.toString());
-		driver.findElement(By.xpath("//input[@value='Add card']")).click();
+		driver.findElement(By.xpath(prop.getProperty("listPage.addCard.xpath"))).click();
 
 		// Dragging and dropping list item from List A to List B
 		Actions action = new Actions(driver);
 		WebElement ele1 = driver
-				.findElement(By.xpath("//h2[text()='List A']/ancestor::div[1]/following-sibling::div[1]/a[1]"));
+				.findElement(By.xpath(prop.getProperty("listPage.listAItem.xpath")));
 		Point p1 = ele1.getLocation();
 		int x1 = p1.getX();
 		int y1 = p1.getY();
 		WebElement ele2 = driver
-				.findElement(By.xpath("//h2[text()='List B']/ancestor::div[1]/following-sibling::div[2]/a[1]"));
+				.findElement(By.xpath(prop.getProperty("listPage.listBBox.xpath")));
 		action.dragAndDrop(ele1, ele2).build().perform();
 
 		// Printing X and Y co-ordinates
-		Point p2 = driver.findElement(By.xpath("//h2[text()='List B']/ancestor::div[1]/following-sibling::div[2]/a[1]"))
+		Point p2 = driver.findElement(By.xpath(prop.getProperty("listPage.listBItem.xpath")))
 				.getLocation();
 		int x2 = p2.getX();
 		int y2 = p2.getY();
@@ -103,11 +103,11 @@ public class Demo {
 	@AfterClass
 	public void logOutAndClose() {
 		Duration twoSecond = Duration.ofSeconds(2);
-		driver.findElement(By.xpath("//button[@data-testid= 'header-member-menu-button']")).click();
+		driver.findElement(By.xpath(prop.getProperty("headerMenu.profileIcon.xpath"))).click();
 		driver.manage().timeouts().implicitlyWait(twoSecond);
-		driver.findElement(By.xpath("//span[text()='Log out']")).click();
+		driver.findElement(By.xpath(prop.getProperty("headerMenu.logOut.xpath"))).click();
 		driver.manage().timeouts().implicitlyWait(twoSecond);
-		driver.findElement(By.xpath("//span[text()='Log out']")).click();
+		driver.findElement(By.xpath(prop.getProperty("headerMenu.logOut.xpath"))).click();
 		driver.close();
 	}
 }
